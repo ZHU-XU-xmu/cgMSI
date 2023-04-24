862fa42e241fde8d26c451010b774d536cd3a024
ZHU-XU-xmu <76027709+ZHU-XU-xmu@users.noreply.github.com>
2023-04-24 12:34:31 +0800

    Update process_alleles_name.py

--- LIB/process_alleles_name.py
+++ LIB/process_alleles_name.py
@@ -1,8 +1,7 @@
 import pandas as pd
 
 def rename_Locus(path):
-    #输入cgMLST下载的tsv文件地址
-    #将位点重新编号，后期方便统一处理
+
     Locus = pd.read_csv(path,sep='\t')
     # print(Locus["Locus"])
     LocusName = Locus["Locus"].tolist()
